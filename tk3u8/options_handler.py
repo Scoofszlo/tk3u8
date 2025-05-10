539d872507b94ae6e1ab43c443c39f55822395bd
Scoofszlo <scoofszlo@gmail.com>
2025-05-10 10:17:44 +0800

    Rename variables

--- tk3u8/options_handler.py
+++ tk3u8/options_handler.py
@@ -8,19 +8,19 @@ from tk3u8.path_initializer import PathInitializer
 class OptionsHandler:
     def __init__(self) -> None:
         self._paths_initializer = PathInitializer()
-        self._args: dict = {}
-        self._config: dict = self._load_config_values()
+        self._args_values: dict = {}
+        self._config_values: dict = self._load_config_values()
 
     def get_option_val(self, key) -> Optional[str | int]:
         try:
             key_map = {
-                OptionKey.SESSIONID_SS: lambda: self._config[OptionKey.SESSIONID_SS.value],
-                OptionKey.TT_TARGET_IDC: lambda: self._config[OptionKey.TT_TARGET_IDC.value],
-                OptionKey.PROXY: lambda: self._args[OptionKey.PROXY.value] or self._config[OptionKey.PROXY.value],
-                OptionKey.USERNAME: lambda: self._args[OptionKey.USERNAME.value],
-                OptionKey.QUALITY: lambda: self._args[OptionKey.QUALITY.value.lower()],
-                OptionKey.WAIT_UNTIL_LIVE: lambda: self._args[OptionKey.WAIT_UNTIL_LIVE.value],
-                OptionKey.TIMEOUT: lambda: self._args[OptionKey.TIMEOUT.value],
+                OptionKey.SESSIONID_SS: lambda: self._config_values[OptionKey.SESSIONID_SS.value],
+                OptionKey.TT_TARGET_IDC: lambda: self._config_values[OptionKey.TT_TARGET_IDC.value],
+                OptionKey.PROXY: lambda: self._args_values[OptionKey.PROXY.value] or self._config_values[OptionKey.PROXY.value],
+                OptionKey.USERNAME: lambda: self._args_values[OptionKey.USERNAME.value],
+                OptionKey.QUALITY: lambda: self._args_values[OptionKey.QUALITY.value.lower()],
+                OptionKey.WAIT_UNTIL_LIVE: lambda: self._args_values[OptionKey.WAIT_UNTIL_LIVE.value],
+                OptionKey.TIMEOUT: lambda: self._args_values[OptionKey.TIMEOUT.value],
             }
             if key in key_map:
                 return key_map[key]()
@@ -31,14 +31,14 @@ class OptionsHandler:
     def save_args_values(self, *args, **kwargs) -> None:
         for arg in args:
             if isinstance(arg, dict):
-                self._args.update(arg)
+                self._args_values.update(arg)
             else:
                 raise TypeError(f"Argument {arg} is not a dict.")
 
         for key, value in kwargs.items():
             for option_key in list(OptionKey):
                 if key in option_key.value:
-                    self._args.update({key: value})
+                    self._args_values.update({key: value})
 
     def _load_config_values(self) -> dict:
         try:
