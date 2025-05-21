6e7779fe67c49b24ee9594bd9aeeb07bb4e74d93
Scoofszlo <scoofszlo@gmail.com>
2025-05-21 16:15:25 +0800

    Change how `PathInitializer` is initialized (#26)
    * Change initialization logic of `PathInitializer`
    
    * Update class initialization at the model
    
    * Make `_set_base_dir` as internal method
    
    * Add some comments

--- tk3u8/path_initializer.py
+++ tk3u8/path_initializer.py
@@ -4,14 +4,25 @@ from tk3u8.constants import DEFAULT_CONFIG
 
 
 class PathInitializer:
+    """
+    Singleton class to initialize and manage important file and directory
+    paths for the application
+    """
     _instance = None
 
-    def __new__(cls) -> 'PathInitializer':
+    def __new__(cls, *args, **kwargs) -> 'PathInitializer':
         if not cls._instance:
             cls._instance = super(PathInitializer, cls).__new__(cls)
         return cls._instance
 
-    def set_base_dir(self, base_dir) -> None:
+    def __init__(self, base_dir=None):
+        # Prevent re-initialization if already initialized
+        if not hasattr(self, "_initialized"):
+            self._set_base_dir(base_dir)
+            self._initialized = True
+
+    def _set_base_dir(self, base_dir) -> None:
+        # Set up main directory and file paths
         self.PROGRAM_DATA_DIR = base_dir if base_dir else "user_data"
         self.STREAM_DATA_FILE = os.path.join(self.PROGRAM_DATA_DIR, "stream_data.json")
         self.CONFIG_FILE_PATH = os.path.join(self.PROGRAM_DATA_DIR, "config.toml")
