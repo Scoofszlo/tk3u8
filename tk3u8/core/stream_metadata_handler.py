6bec9c47c211038e29ec4abc35d4dc697a8bb70f
Scoofszlo <scoofszlo@gmail.com>
2025-05-25 12:55:05 +0800

    Improve `StreamMetadataHandler` methods

--- tk3u8/core/stream_metadata_handler.py
+++ tk3u8/core/stream_metadata_handler.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List
 from tk3u8.constants import OptionKey, StreamLink
 from tk3u8.core.extractor import APIExtractor, Extractor, WebpageExtractor
 from tk3u8.core.helper import is_user_exists, is_user_live, is_username_valid
@@ -29,9 +29,8 @@ class StreamMetadataHandler:
         self._username: str | None = None
         self._quality: str | None = None
 
-    def get_stream_link(self) -> Optional[StreamLink]:
-        if not self._quality:
-            return None
+    def get_stream_link(self) -> StreamLink:
+        assert isinstance(self._quality, str)
 
         try:
             if self._quality in self._stream_links:
@@ -41,6 +40,24 @@ class StreamMetadataHandler:
             raise QualityNotAvailableError()
 
     def initialize_data(self) -> None:
+        self._process_data()
+
+    def update_data(self) -> None:
+        self._process_data()
+
+    def _get_username(self) -> str:
+        username = self._options_handler.get_option_val(OptionKey.USERNAME)
+        assert isinstance(username, (str, type(None)))
+
+        if not username:
+            raise NoUsernameEnteredError()
+
+        if not is_username_valid(username):
+            raise InvalidUsernameError(username)
+
+        return username
+
+    def _process_data(self) -> None:
         if not self._username:
             self._username = self._get_username()
 
@@ -71,34 +88,6 @@ class StreamMetadataHandler:
                     print(f"Extractor #{idx+1} ({extractor.__class__.__name__}) failed due to {type(e).__name__}. No more extractors to be used. The program will now exit.")
                     exit()
 
-    def update_data(self) -> None:
-        assert isinstance(self._username, str)
-
-        for extractor_class in self._extractor_classes:
-            extractor = extractor_class(self._username, self._request_handler)
-
-            self._source_data = self._get_and_validate_source_data(extractor, extractor_class)
-            self._live_status_code = extractor.get_live_status_code(self._source_data)
-
-            if not is_user_live(self._live_status_code):
-                break
-
-            self._stream_data = extractor.get_stream_data(self._source_data)
-            self._stream_links = extractor.get_stream_links(self._stream_data)
-            self._quality = self._get_and_validate_quality()
-
-    def _get_username(self) -> str:
-        username = self._options_handler.get_option_val(OptionKey.USERNAME)
-        assert isinstance(username, (str, type(None)))
-
-        if not username:
-            raise NoUsernameEnteredError()
-
-        if not is_username_valid(username):
-            raise InvalidUsernameError(username)
-
-        return username
-
     def _get_and_validate_source_data(self, extractor: Extractor, extractor_class: type[Extractor]) -> dict:
         source_data: dict = extractor.get_source_data()
 
@@ -107,7 +96,7 @@ class StreamMetadataHandler:
 
         return source_data
 
-    def _get_and_validate_quality(self):
+    def _get_and_validate_quality(self) -> str:
         quality = self._options_handler.get_option_val(OptionKey.QUALITY)
         assert isinstance(quality, str)
 
