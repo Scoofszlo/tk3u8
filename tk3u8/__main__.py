a88c86f0f02323ccaf28397090c3fea3a8b9e43f
Scoofszlo <scoofszlo@gmail.com>
2025-05-28 17:34:40 +0800

    Setup logging

--- tk3u8/__main__.py
+++ tk3u8/__main__.py
@@ -1,5 +1,9 @@
+import logging
+import os
+from datetime import datetime
 from tk3u8.args_handler import ArgsHandler
 from tk3u8.core.model import Tk3u8
+from tk3u8 import logger
 
 
 if __name__ == "__main__":
@@ -11,6 +15,21 @@ if __name__ == "__main__":
     proxy = args.proxy
     wait_until_live = args.wait_until_live
     timeout = args.timeout
+
+    logger.setLevel(logging.DEBUG)
+
+    log_directory = os.path.join("user_data", "logs")
+
+    if not os.path.exists(log_directory):
+        os.mkdir(log_directory)
+
+    log_filename = f"logs-{datetime.now().strftime('%Y%m%d')}.log"
+    log_file = os.path.join(log_directory, log_filename)
+
+    file_handler = logging.FileHandler(log_file, encoding='utf-8')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s | %(levelname)s | %(name)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S%z'))
+    logger.addHandler(file_handler)
 
     tk3u8 = Tk3u8()
     tk3u8.set_proxy(proxy)
